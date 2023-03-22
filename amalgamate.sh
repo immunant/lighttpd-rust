80fdc06a331c4e4ce00a7e06dd56496921e9ac7b
Khyber Sen <kkysen@gmail.com>
2023-03-21 19:21:28 -0700

    Added the transpilation step to `amalgamate.sh` into a separate `lighttpd_rust_amalgamated`.

--- amalgamate.sh
+++ amalgamate.sh
@@ -2,7 +2,12 @@
 
 set -euox pipefail
 
+rust_dir=lighttpd_rust_amalgamated
+
 cmake -Wno-dev
 make clean
 bear -- make lighttpd
 ./amalgamate.mjs | "${SHELL}" -euox pipefail
+c2rust transpile --overwrite-existing --emit-build-files --binary lighttpd_amalgamated --output-dir ${rust_dir} amalgamated.compile_commands.json
+cp {rust,${rust_dir}}/build.rs
+(cd "${rust_dir}" && cargo build)
