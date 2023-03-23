4d566a7b273c24bbe8ce259dd0a7a0e129c057ff
David Anekstein <da@immunant.com>
2023-03-23 18:36:51 -0400

    cleanup

--- lighttpd_rust_amalgamated/src/main.rs
+++ lighttpd_rust_amalgamated/src/main.rs
@@ -9,12 +9,6 @@
 )]
 #![feature(c_variadic, core_intrinsics, extern_types, label_break_value)]
 #![allow(dead_code)]
-#![allow(mutable_transmutes)]
-#![allow(non_camel_case_types)]
-#![allow(non_snake_case)]
-#![allow(non_upper_case_globals)]
-#![allow(unused_assignments)]
-#![allow(unused_mut)]
 
 #[macro_use] extern crate libc;
 #[macro_use] extern crate c2rust_bitfields;
