45b92f407f112f528c136da53352356b8a826e3e
Khyber Sen <kkysen@gmail.com>
2023-03-25 02:46:09 -0700

    `cargo fmt` `lighttpd_rust_amalgamated`.

--- amalgamate.sh
+++ amalgamate.sh
@@ -11,4 +11,7 @@ make mod_{indexfile,dirlisting,staticfile}
 ./amalgamate.mjs | "${SHELL}" -euox pipefail
 c2rust transpile --overwrite-existing --emit-build-files --binary lighttpd_amalgamated --output-dir ${rust_dir} amalgamated.compile_commands.json
 cp {rust,${rust_dir}}/build.rs
-(cd "${rust_dir}" && cargo build)
+(cd "${rust_dir}"
+    cargo fmt
+    cargo build
+)
