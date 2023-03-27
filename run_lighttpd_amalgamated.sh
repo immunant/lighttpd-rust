c66cfb8e9644bc2e54671021ec91d56ad1085fa4
David Anekstein <da@immunant.com>
2023-03-27 13:26:27 -0400

    add script to run from c2rust directory for static analysis on lighttpd-amalgamated

---
+++ run_lighttpd_amalgamated.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -euo pipefail
+
+# Validate arguments
+if [[ $# -ne 1 ]]; then
+  echo "Usage: $0 MODULE_DIR"
+  exit 1
+fi
+
+# Get the directory of this script
+MODULE_DIR="$1"
+
+# Find the sysroot directory of rustc
+SYSROOT="$(rustc --print sysroot)"
+
+# Find the necessary rlibs
+C2RUST_BITFIELDS=$(find "$MODULE_DIR/target/debug/deps" -name "libc2rust_bitfields*.rlib" -print -quit)
+
+# Print the found rlibs
+echo "Found rlibs:"
+echo "  c2rust_bitfields: $C2RUST_BITFIELDS"
+
+# Run the program with the necessary dependencies
+cargo run --bin c2rust-analyze -- "$MODULE_DIR/src/main.rs"\
+  --edition 2021 \
+  --crate-type rlib \
+  -L "dependency=$MODULE_DIR/target/debug/deps" \
+  -L "$SYSROOT/lib/rustlib/x86_64-unknown-linux-gnu/lib" \
+  --extern c2rust_bitfields="$C2RUST_BITFIELDS"
